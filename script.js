227523511d9870d16c3caa7f4b3e74e30a72f3e9
nacho1001tt <nachoxd1001@gmail.com>
2025-03-20 10:14:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ function mostrarSeña(speechText) {
     let nombreArchivo = "desconocido.gif.mp4";
 
     if (speechText.includes("hola")) {
-        nombreArchivo = "hola.gif,mp4";
+        nombreArchivo = "hola.gif.mp4";
     } else if (speechText.includes("como estas")) {
-        nombreArchivo = "como estas.gif.mp4";
+        nombreArchivo = "cómo estás.gif.mp4";
     }
 
     function reproducirGif() {
